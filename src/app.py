1e97e6d3522dc650bd520ddb9e2daccb337aedc0
Admin <admin@apple.com>
2017-07-18 00:07:54 -0400

    refactored main sass file

--- src/app.py
+++ src/app.py
@@ -7,25 +7,31 @@ app = Flask(__name__)
 def home():
     return render_template('layout.html')
 
+
 @app.route('/rooms')
 def rooms():
     pass
+
 
 @app.route('/rates')
 def rates():
     pass
 
+
 @app.route('/gallery')
 def gallery():
     pass
+
 
 @app.route('/booking')
 def booking():
     pass
 
+
 @app.route('/testimonials')
 def testimonials():
     pass
+
 
 @app.route('/contact')
 def contact():
